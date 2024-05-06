2b007ef9dc1add60342168ad774b1c0822c1620c
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-05-06 12:24:07 +0700

    Feat: View Update Persetujuan Saldo Kas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:emosque_mobile/views/persetujuanSaldoKas.dart';
+// import 'package:emosque_mobile/views/persetujuanSaldoKas.dart';
+import 'package:emosque_mobile/views/updatePersetujuanSaldoKas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PersetujuanSaldoKas(),
+      home: UpdatePersetujuanSaldoKas(),
     );
   }
 }
---
+++ lib/widgets/button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Button extends StatelessWidget {
+  final text;
+  const Button({
+    super.key,required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.greenAccent,
+          width: 2
+        ),
+        borderRadius: BorderRadius.circular(5)
+      ),
+      width: 309,
+      height: 46,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(7))),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              text,
+              style: GoogleFonts.poppins(
+                  color: Colors.greenAccent[700],
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
