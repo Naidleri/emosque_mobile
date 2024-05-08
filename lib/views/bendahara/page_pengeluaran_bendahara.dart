ae938065fadb13d79be45b32b0c16f4d439d8d9a
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-05-08 15:12:48 +0700

    +FE homepage bendahara, page pengeluaran bendahara

---
+++ lib/views/bendahara/page_pengeluaran_bendahara.dart
@@ -0,0 +1,154 @@
+import 'package:emosque_mobile/widgets/picker_date.dart';
+import 'package:flutter/material.dart';
+
+class PengeluaranBendaharaPage extends StatefulWidget {
+  const PengeluaranBendaharaPage({super.key});
+
+  @override
+  State<PengeluaranBendaharaPage> createState() =>
+      _PengeluaranBendaharaPageState();
+}
+
+class _PengeluaranBendaharaPageState extends State<PengeluaranBendaharaPage> {
+
+  Widget search() {
+    return Container(
+      height: 60,
+      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      padding: const EdgeInsets.only(left: 10),
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10)),
+      child: const Row(
+        children: [
+          // Icon
+          Icon(Icons.search, color: Colors.black54),
+          SizedBox(width: 10.0),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 12.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Cari transaksi...',
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget cardHistoryPengeluaran(String judul, String tanggal, String uang) {
+    return InkWell(
+      onLongPress: () {},
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+            border: Border(
+                top: BorderSide(color: Colors.grey),
+                bottom: BorderSide(color: Colors.grey))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  judul,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  tanggal,
+                  style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            Text(
+              uang,
+              style: const TextStyle(fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Pengeluaran",
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.w700, color: Colors.green),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          const PickerDate(),
+          search(),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000"),
+          cardHistoryPengeluaran(
+              "Santunan anak yatim", "15 April 2024", "Rp 550.000")
+        ],
+      ),
+      bottomSheet: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 90,
+            color: Colors.white,
+          ),
+          Positioned(
+              top: 10,
+              right: 20,
+              left: 20,
+              bottom: 25,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10.0), // Set corner radius
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  "Tambahkan Pengeluaran",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/picker_date.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PickerDate extends StatefulWidget {
+  const PickerDate({super.key});
+
+  @override
+  _PickerDateState createState() => _PickerDateState();
+}
+class _PickerDateState extends State<PickerDate> {
+  int pilihBulan = 0;
+  int pilihTahun = 0;
+
+  final Map<int, String> _bulan = {
+    1: "Januari",
+    2: "Februari",
+    3: "Maret",
+    4: "April",
+    5: "Mei",
+    6: "Juni",
+    7: "Juli",
+    8: "Agustus",
+    9: "September",
+    10: "Oktober",
+    11: "November",
+    12: "Desember"
+  };
+
+  Widget filterTanggal(BuildContext context) {
+    return Container(
+      height: 60,
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.only(left: 10),
+      decoration: BoxDecoration(
+          color: Colors.green[400], borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () {
+          _showPicker();
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                const Icon(
+                  Icons.date_range,
+                  size: 26,
+                  color: Colors.white,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 5),
+                  child: Text(
+                    "${_bulan[pilihBulan]} $pilihTahun",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                )
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 16,
+                child: Icon(
+                  Icons.arrow_right,
+                  color: Colors.white,
+                  size: 24,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showPicker() {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (_) => Row(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 250, // Set your desired height
+              child: CupertinoPicker(
+                backgroundColor: Colors.green,
+                itemExtent: 50,
+                scrollController: FixedExtentScrollController(
+                  initialItem: pilihBulan - 1,
+                ),
+                onSelectedItemChanged: (int value) {
+                  setState(() {
+                    pilihBulan = value + 1;
+                  });
+                },
+                children: _bulan.values
+                    .map((bulan) => Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text(
+                            bulan,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                    ))
+                    .toList(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 250, // Set your desired height
+              child: CupertinoPicker(
+                backgroundColor: Colors.green,
+                itemExtent: 50,
+                scrollController: FixedExtentScrollController(
+                  initialItem: pilihTahun - 2000,
+                ),
+                onSelectedItemChanged: (int value) {
+                  setState(() {
+                    pilihTahun = value + 2000;
+                  });
+                },
+                children: _generateTahunItems(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _generateTahunItems() {
+    List<Widget> tahunItems = [];
+    for (int tahun = 2000; tahun <= 5000; tahun++) {
+      tahunItems.add(Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Text(
+          "$tahun",
+          style: TextStyle(
+            color: Colors.white,
+          ), // Center text horizontally
+        ),
+      ));
+    }
+    return tahunItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return filterTanggal(context);
+  }
+}
