a10d3cd8b46f034c75baf95a472dd6f0b27b0069
ZeinIM <irfanmzein1604@gmail.com>
2024-05-16 01:53:01 +0700

    jemaat

---
+++ lib/views/jamaah/kalenderjamaah.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class KalenderJamaah extends StatefulWidget {
+  const KalenderJamaah({super.key});
+
+  @override
+  State<KalenderJamaah> createState() => _MyKalenderJamaahState();
+}
+
+class _MyKalenderJamaahState extends State<KalenderJamaah> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Kalender',
+            style: TextStyle(
+                color: Color(0xff06d773),
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: ListView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 7,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: 350,
+                  child: TableCalendar(
+                    locale: "en_US",
+                    rowHeight: 43,
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                      titleTextStyle: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                      ),
+                      leftChevronIcon: Icon(
+                        Icons.chevron_left,
+                        color: Colors.black,
+                      ),
+                      rightChevronIcon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.black,
+                      ),
+                    ),
+                    calendarStyle: CalendarStyle(
+                      defaultTextStyle: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.black,
+                      ),
+                      weekendTextStyle: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.red,
+                      ),
+                      selectedTextStyle: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                      todayTextStyle: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    selectedDayPredicate: (day) =>
+                        isSameDay(day, DateTime.now()),
+                    availableGestures: AvailableGestures.all,
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime.utc(2000, 01, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 15),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(right: 8),
+                            child: const Text(
+                              "2",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.80,
+                            child: Container(
+                              padding: EdgeInsets.only(
+                                  top: 7, bottom: 8, left: 30),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(5),
+                                  color: const Color.fromARGB(
+                                      255, 55, 163, 165)),
+                              child: Text(
+                                "Hari Qurban",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 14, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const KalenderJamaah());
+}
