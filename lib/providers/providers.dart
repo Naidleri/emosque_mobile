a0d0d0cf4e3355fe058a4781fc2eddaa923a1e2e
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-05-21 21:03:44 +0700

    fix: resolve dependency

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,6 @@
+import 'package:emosque_mobile/services/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 import '../models/models.dart';
 import 'package:flutter/material.dart';
 
