2558c43c083b0579b6111322ce34c0ba99b013d8
adyfp24 <adyfp24@gmail.com>
2024-05-23 09:02:35 +0700

    feat: get zakat method

--- lib/providers/qurban_provider.dart
+++ lib/providers/qurban_provider.dart
@@ -20,10 +20,38 @@ class QurbanProvider extends ChangeNotifier {
       final storedToken = _secureStorage.read(key: 'token');
       final data = await _qurbanService.createQurban(newQurban, storedToken); 
     }catch(e){
-      print('Error update saldo kas: $e');
-      throw Exception('Failed to update saldo kas $e');
+      print('Error create qurban: $e');
+      throw Exception('Failed to create qurban $e');
     }
     _isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> getAllQurban () async {
+    _isLoading = true;
+    notifyListeners();
+    try{
+      final storedToken = _secureStorage.read(key: 'token');
+      final data = await _qurbanService.getAllQurban(storedToken);
+    }catch(e){
+        print('Error get allqurban: $e');
+      throw Exception('Failed to get all qurban $e');
+    }
+     _isLoading = true;
+    notifyListeners();
+  }
+
+  Future<void> getQurbanById (int idQurban) async {
+    _isLoading = true;
+    notifyListeners();
+    try{
+      final storedToken = _secureStorage.read(key: 'token');
+      final data = await _qurbanService.getQurbanById(idQurban, storedToken);
+    }catch(e){
+        print('Error get qurban: $e');
+      throw Exception('Failed to get qurban $e');
+    }
+     _isLoading = true;
     notifyListeners();
   }
 }
--- lib/services/qurban_service.dart
+++ lib/services/qurban_service.dart
@@ -16,4 +16,34 @@ class QurbanService {
       return ApiHelper.handleError(e);
     }
   }
+
+  Future<Qurban> getAllQurban (String token) async {
+    final url = Uri.parse('${ApiHelper.baseUrl}/qurban');
+    try{
+      final response = await http.get(
+        url,
+        headers: ApiHelper.getHeaders(token),
+      );
+      final responseData = ApiHelper.handleResponse(response);
+      final allQurbanData = Qurban.fromJson(responseData['data']);
+      return allQurbanData;
+    }catch(e){
+      return ApiHelper.handleError(e);
+    }
+  }
+
+  Future<Qurban> getQurbanById (int idQurban, String token) async {
+    final url = Uri.parse('${ApiHelper.baseUrl}/qurban/$idQurban');
+    try{
+      final response = await http.get(
+        url,
+        headers: ApiHelper.getHeaders(token),
+      );
+      final responseData = ApiHelper.handleResponse(response);
+      final qurbanData = Qurban.fromJson(responseData['data']);
+      return qurbanData;
+    }catch(e){
+      return ApiHelper.handleError(e);
+    }
+  }
 }
