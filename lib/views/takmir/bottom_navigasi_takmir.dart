a32f98b6c7caa79e68ab699109ce959a14aa47b7
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-05-29 00:57:01 +0700

    fix: nav takmir

---
+++ lib/views/takmir/bottom_navigasi_takmir.dart
@@ -0,0 +1,68 @@
+import 'package:emosque_mobile/views/bendahara/persetujuan_saldo_kas.dart';
+import 'package:emosque_mobile/views/bendahara/profile_bendahara.dart';
+import 'package:emosque_mobile/views/takmir/laporan_page.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigasiTakmir extends StatefulWidget {
+  const BottomNavigasiTakmir({super.key});
+
+  @override
+  State<BottomNavigasiTakmir> createState() =>
+      _BottomNavigasiTakmirState();
+}
+
+class _BottomNavigasiTakmirState extends State<BottomNavigasiTakmir> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    const LaporanPage(),
+    ApprovePage(),
+    const ProfileBendahara()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Color.fromARGB(255, 6, 215, 115),
+              size: 28,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.insert_chart,
+              color: Color.fromARGB(255, 6, 215, 115),
+              size: 28,
+            ),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: Color.fromARGB(255, 6, 215, 115),
+              size: 28,
+            ),
+            label: 'Profile',
+          ),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
---
+++ lib/views/takmir/profile_takmir.dart
@@ -0,0 +1,68 @@
+import 'package:emosque_mobile/widgets/fillButton.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:badges/badges.dart' as badges;
+
+class ProfileTakmir extends StatelessWidget {
+  const ProfileTakmir({super.key});
+  Widget text(String judul,String text, BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            judul,
+            style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black),
+          ),
+          const SizedBox(height: 5),
+          Container(
+            decoration: BoxDecoration(border: Border.all(width: 2, color: Colors.green), borderRadius: BorderRadius.circular(10)),
+            padding: const EdgeInsets.only(top: 16, left: 15),
+            height: 60,
+            width: MediaQuery.of(context).size.width * 0.85,
+            child: Text(text, style: GoogleFonts.poppins(
+                fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: GoogleFonts.poppins(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.greenAccent[700]),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+            badges.Badge(
+              badgeStyle: const badges.BadgeStyle(badgeColor: Colors.green),
+              position: badges.BadgePosition.bottomStart(),
+              badgeContent: IconButton(onPressed: () {}, icon: const Icon(Icons.edit, color: Colors.white,)),
+              child: const CircleAvatar(radius: 60, 
+                backgroundImage: NetworkImage('https://example.com/your-image-url.jpg'),),
+            ),
+            text('Nama', 'Ace Anugrah', context),
+            text('Username', 'firdhaa.c', context),
+            text('Email', 'firdhaac02@gmail.com', context),
+            text('Role pengurus', 'Jamaah', context),
+            const SizedBox(height: 20,),
+            fillButton(text: 'Log out', onPressed: (){}),
+          ],
+        ),
+      ),
+    );
+  }
+}
