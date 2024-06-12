01f54d2a2eef624d56f63dbe35f178c40a0bcae3
adyfp24 <adyfp24@gmail.com>
2024-06-13 01:29:42 +0700

    feat: integration for read all yayasan zakat

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => LaporanProvider(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => YayasanZProvider(),
+        )
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -111,7 +114,8 @@ class MyApp extends StatelessWidget {
           '/createQurbanSekertaris': (context) => CreateQurbanSekertaris(),
           '/createPenerimaZakatSekertaris': (context) =>
               const CreatePenerimaZakatSekertaris(),
-          '/createPenerimaQurbanSekertaris': (context) => const CreatePenerimaQurbanSekertaris(),
+          '/createPenerimaQurbanSekertaris': (context) =>
+              const CreatePenerimaQurbanSekertaris(),
           '/updatePerizinanSekertaris': (context) =>
               const UpdatePerizinanSekertaris(),
           '/updatePenerimaZakatSekertaris': (context) =>
@@ -124,7 +128,8 @@ class MyApp extends StatelessWidget {
           '/yayasanSekertaris': (context) => Yayasan(),
           '/calenderSekertaris': (context) => const MyCalenderSeker(),
           // Takmir
-          '/homepageTakmir': (context) => BottomNavigasiTakmir(key: takmirNavigationKey),
+          '/homepageTakmir': (context) =>
+              BottomNavigasiTakmir(key: takmirNavigationKey),
           '/readLaporanTakmir': (context) => const ReadLaporanTakmir(),
         },
         home: const LoginPage(),
--- lib/views/sekertaris/read_yayasan_sekertaris.dart
+++ lib/views/sekertaris/read_yayasan_sekertaris.dart
@@ -1,8 +1,10 @@
 import 'package:emosque_mobile/models/models.dart';
 import 'package:emosque_mobile/views/sekertaris/read_detail_penerima_qurban.dart';
 import 'package:emosque_mobile/views/sekertaris/read_detail_penerima_zakat.dart';
+import 'package:emosque_mobile/providers/providers.dart'; // Ensure the provider is imported
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class ReadYayasanSekertaris extends StatefulWidget {
   const ReadYayasanSekertaris({super.key});
@@ -19,20 +21,20 @@ class _ReadYayasanSekertarisState extends State<ReadYayasanSekertaris>
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this, initialIndex: 0);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<YayasanZProvider>(context, listen: false).getAllYayasan();
+    });
   }
 
-  Widget cardYayasan(String nama, String waktu, String jenis,
-      VoidCallback onTap, BuildContext context) {
+  Widget cardYayasan(YayasanZakat yayasan, VoidCallback onTap, BuildContext context) {
     return InkWell(
       onTap: onTap,
       child: Container(
         height: 150,
         decoration: const BoxDecoration(
           border: Border(
-            top:
-                BorderSide(color: Color.fromRGBO(172, 172, 172, 1), width: 0.7),
-            bottom:
-                BorderSide(color: Color.fromRGBO(172, 172, 172, 1), width: 0.7),
+            top: BorderSide(color: Color.fromRGBO(172, 172, 172, 1), width: 0.7),
+            bottom: BorderSide(color: Color.fromRGBO(172, 172, 172, 1), width: 0.7),
           ),
         ),
         child: Column(
@@ -40,17 +42,17 @@ class _ReadYayasanSekertarisState extends State<ReadYayasanSekertaris>
             ListTile(
               contentPadding: const EdgeInsets.only(left: 25, right: 25),
               title: Text(
-                nama,
+                yayasan.namaYayasan,
                 style: GoogleFonts.poppins(
                     fontSize: 16, fontWeight: FontWeight.w500),
               ),
               subtitle: Text(
-                waktu,
+                yayasan.tanggal,
                 style: GoogleFonts.poppins(
                     fontSize: 12, fontWeight: FontWeight.w400),
               ),
               trailing: Text(
-                jenis,
+                '${yayasan.rekapanBeras} Kg',
                 style: GoogleFonts.poppins(
                     fontSize: 16, fontWeight: FontWeight.w400),
               ),
@@ -155,41 +157,60 @@ class _ReadYayasanSekertarisState extends State<ReadYayasanSekertaris>
         children: [
           Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                cardYayasan('Agus', '10 Des 2020', '11 Kg', () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => const ReadDetailPenerimaZakat()));
-                }, context),
-                cardYayasan('Agus', '10 Des 2020', '11 Kg', () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => const ReadDetailPenerimaZakat()));
-                }, context)
-              ],
+            child: Consumer<YayasanZProvider>(
+              builder: (context, provider, child) {
+                if (provider.isLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (provider.yayasanZakat.isEmpty) {
+                  return Center(child: Text('No data available.'));
+                }
+                return ListView.builder(
+                  itemCount: provider.yayasanZakat.length,
+                  itemBuilder: (context, index) {
+                    final yayasan = provider.yayasanZakat[index];
+                    return cardYayasan(yayasan, () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ReadDetailPenerimaZakat(yayasanZakat: yayasan),
+                          
+                        ),
+                      );
+                    }, context);
+                  },
+                );
+              },
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
-                cardYayasan('Agus', '10 Des 2020', '11 Kg', () {
-                  Navigator.push(
+                cardYayasan(
+                  YayasanZakat(0, 'Agus', '10 Des 2020', 0, 11, ''),
+                  () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (_) =>
-                              const ReadDetailPenerimaQurban()));
-                }, context),
-                cardYayasan('Agus', '10 Des 2020', '11 Kg', () {
-                  Navigator.push(
+                        builder: (_) => ReadDetailPenerimaQurban(),
+                      ),
+                    );
+                  },
+                  context,
+                ),
+                cardYayasan(
+                  YayasanZakat(0, 'Agus', '10 Des 2020', 0, 11, ''),
+                  () {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (_) =>
-                              const ReadDetailPenerimaQurban()));
-                }, context),
+                        builder: (_) => ReadDetailPenerimaQurban(),
+                      ),
+                    );
+                  },
+                  context,
+                ),
               ],
             ),
           ),
@@ -200,8 +221,8 @@ class _ReadYayasanSekertarisState extends State<ReadYayasanSekertaris>
         margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)),
             backgroundColor: Colors.green[700],
           ),
           onPressed: () {
